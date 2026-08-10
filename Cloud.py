76f25273c0b2da324fa3a3ce0f6bcfd7e24239ef
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-08-10 12:56:27 +0300

    Implement home route with city input and update about route description

--- Cloud.py
+++ Cloud.py
@@ -1,7 +1,20 @@
-from flask import Flask  # error says that flask is unresolved
+# error says that flask is unresolved. render_template is a function and request is an object
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route("/about")
+# this route is allowed to receive both GET and POST requests.
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return "CloudNine says hello!"
+    city = "London"
+    if request.method == "POST":
+        city = request.form["city"]
+        print(city)
+
+    # message is an argument that
+    return render_template("cloud.html", message="Welcome to CloudNine!!", city=city)
+
+
+@app.route("/about")
+def about():
+    return "CloudNine is a weather app built with Flaskkkk"
